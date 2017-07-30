602801d344d1339198421da1cd9cc101e0efee9d
coolklokan <coolklokan@gmail.com>
2017-07-30 11:51:56 +0200

    Router changes for cron

--- app/model/router/Router.php
+++ app/model/router/Router.php
@@ -1,31 +1,43 @@
 <?php
 
 
+use Nette\Application\Routers\CliRouter;
 use Nette\Application\Routers\Route;
 use Nette\Application\Routers\RouteList;
 
 class Router {
+    private $consoleMode;
+
+    public function __construct(bool $consoleMode) {
+        $this->consoleMode = $consoleMode;
+    }
 
     /**
      * @return \Nette\Application\IRouter
      */
     public function createRouter() {
         $routes = new RouteList();
+        if ($this->consoleMode) {
 
-        $apimethod = BasePresenter::API_METHOD;
-        $htmlmethod = BasePresenter::HTML_METHOD;
+            $routes[] = new CliRouter([
+                "action" => "Default:default",
+            ]);
+        } else {
+            $apimethod = BasePresenter::API_METHOD;
+            $htmlmethod = BasePresenter::HTML_METHOD;
 
-        $api = "<method {$apimethod}>/";
-        $both = "[<method={$htmlmethod} {$htmlmethod}|{$apimethod}>/]";
-        $html = "[<method={$htmlmethod} {$htmlmethod}>/]";
-        //$routes[] = new Route("LogIn", "User:logIn");
-        //$routes[] = new Route("SignUp", "User:signUp");
-        //$routes[] = new Route("SignOut", "User:signOut");
-        $type = UserPresenter::AVAILABILITY_TYPE_KEYWORD;
-        $types = implode("|", UserPresenter::AVAILABILITY_TYPES);
-        $routes[] = new Route($both . "<presenter user>/<action sign-up|sign-out|log-in>");
-        $routes[] = new Route($api . "<presenter user>/<action check-availability>/<{$type} {$types}>");
-        $routes[] = new Route($html . "<presenter>/<action>", "HomePage:default");
+            $api = "<method {$apimethod}>/";
+            $both = "[<method={$htmlmethod} {$htmlmethod}|{$apimethod}>/]";
+            $html = "[<method={$htmlmethod} {$htmlmethod}>/]";
+            //$routes[] = new Route("LogIn", "User:logIn");
+            //$routes[] = new Route("SignUp", "User:signUp");
+            //$routes[] = new Route("SignOut", "User:signOut");
+            $type = UserPresenter::AVAILABILITY_TYPE_KEYWORD;
+            $types = implode("|", UserPresenter::AVAILABILITY_TYPES);
+            $routes[] = new Route($both . "<presenter user>/<action sign-up|sign-out|log-in>");
+            $routes[] = new Route($api . "<presenter user>/<action check-availability>/<{$type} {$types}>");
+            $routes[] = new Route($html . "<presenter>/<action>", "HomePage:default");
+        }
         return $routes;
     }
 }
