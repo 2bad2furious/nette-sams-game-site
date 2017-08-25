204fe71a0d0a441fb1fa6a16514bd41467f1a4f0
2bad2furious <coolklokan@gmail.com>
2017-08-25 07:55:38 +0200

    Header markup change

--- app/components/HeaderControl.php
+++ app/components/HeaderControl.php
@@ -6,10 +6,12 @@ use Nette\Security\User;
 
 class HeaderControl extends BaseControl {
 
-    public function render(): void {
+    public function render(bool $isAlone): void {
         $template = $this->template;
         $template->isLoggedIn = $this->getPresenter()->getUser()->isLoggedIn();
         $template->identity = @$this->getPresenter()->getUser()->getIdentity();
+        $template->isAlone = $isAlone;
+        $template->isHome = $this->getPresenter()->getName() === "HomePage";
         $template->render();
     }
 
